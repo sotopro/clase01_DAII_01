bfe0dfed75c8afc5f7b79d4a46f9da2deabb1ac0
dsoto1-rbi <dsoto1@rbi.com>
2022-05-25 12:58:40 -0500

    [topic]- implement as an object

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,18 @@
-class App: DownloadListener {
-    override fun onDownloadStarted() {
-        println("Download started")
-    }
 
-    override fun onDownloadComplete(file: String) {
-        println("$file downloaded")
-    }
-
-    override fun onProgressUpdate(progress: Int) {
-        println("$progress% downloaded")
-    }
-
-}
 fun main() {
-    val downloaderListener = App()
     val downloader = Downloader()
-    downloader.downloadListener = downloaderListener
+    downloader.downloadListener = object : DownloadListener {
+        override fun onDownloadStarted() {
+            println("Download started")
+        }
+
+        override fun onDownloadComplete(file: String) {
+            println("$file downloaded")
+        }
+
+        override fun onProgressUpdate(progress: Int) {
+            println("$progress% downloaded")
+        }
+    }
     downloader.downloadFile("newSong.mp3")
 }
